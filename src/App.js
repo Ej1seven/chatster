7778b4d2ea4c9bec4a0d3305a0120e418a0a6382
Ej1seven <erik.jamison.hunter@gmail.com>
2021-09-01 23:29:08 -0500

    changed login background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Switch, Route } from "react-router-dom";
+import Login from "../../client/src/pages/Login/login";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <Switch>
+      <Route path="/">
+        <Login/>
+      </Route>
+    </Switch>
     </div>
   );
 }
---
+++ src/pages/Login/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./login.css";
+
+function Login() {
+    return (
+     <div className="splash-screen">
+       Chatster
+     </div>
+    );
+  }
+  
+  export default Login;
