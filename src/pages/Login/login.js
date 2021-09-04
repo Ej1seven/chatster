7a8420e97f9ec8e85a8a385f912f89763103d5bd
Ej1seven <erik.jamison.hunter@gmail.com>
2021-09-04 15:15:56 -0500

    added form and form animations

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -3,15 +3,28 @@ import logo from "../../photos/logo-white.png";
 import "./login.css";
 
 function Login() {
-    return (
-      <div> 
+  return (
+    <div>
       <div transition-style className="splash-screen">
-      <p className="app-title">Chatster</p>
-      <img alt="logo" className="app-logo" src={logo}/>
-    
+        <p className="app-title">Chatster</p>
+        <img alt="logo" className="app-logo" src={logo} />
+        <form className="rounded flex justify-center flex-col items-center	">
+          <p className="form-title">Chatster</p>
+          <img alt="logo" className="form-logo h-1/3" src={logo} />
+          <input
+            type="text"
+            className="rounded m-6 bg-transparent border-2 border-white"
+            placeholder="Email"
+          />
+          <input
+            type="text"
+            className="rounded m-6 bg-transparent border-2 border-white"
+            placeholder="Password"
+          />
+        </form>
       </div>
-      </div>
-    );
-  }
-  
-  export default Login;
+    </div>
+  );
+}
+
+export default Login;
